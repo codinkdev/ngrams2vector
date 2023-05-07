f7e02b22f8173be76d6a3232668c6b6079287d3b
Lautaro Matas <lmatas@gmail.com>
2023-05-07 17:11:57 +0200

    optimization

--- ngram.py
+++ ngram.py
@@ -12,31 +12,37 @@ class Ngram:
         self.n = n
         self.word = word
         self.symbol_count = symbol_count
-        self.ngrams = self.ngrams()
+        self.ngrams_values = self.ngrams()
         
-        self.dvalues = {}
-        for ngram in self.ngrams:
-            self.dvalues[ngram] = 1
             
     """ Operaciones """
-    # Producto Interno
+    # Producto Interno calculado sobre los conjuntos de valores enteros (indices) de los ngramas
+    # Esto se basa en que los valores ngramas son unicos y que el producto interno es la cantidad de elementos en la interseccion
+    # a = (1,0,0,1) es representad como el conjunto {0,3}
+    # b = (0,1,0,1) es representado como el conjunto {1,3}
+    # a.b = 2 (longitud interseccion de los conjuntos)
     def iproduct(self, other):
-        sum = 0
-        keylist = self._getlist(other)
-        for key in keylist:
-            sum = sum + self.dvalues.get(key,0)*other.dvalues.get(key,0)    
-        return sum
+        intersection = set(self.ngrams_values).intersection(set(other.ngrams_values))
+        return len(intersection)
         
     # Norma del vector
+    # La norma del vector es la raiz cuadrada de la suma de los cuadrados de sus elementos
+    # asumiendo vectores binarios, la norma es la raiz cuadrada de la cantidad de elementos del vector distintos de cero
+    # en la representacion de los ngramas, un elemento distinto de cero es un indice de un ngrama en el conjunto ngram_values
+    # a = (1,0,0,1) es representad como el conjunto {0,3} y su norma es sqrt(2)
     def norm(self):
-        return math.sqrt( len(self.dvalues.keys()))
+        return math.sqrt( len(self.ngrams_values))
     
-    # Comparacion de dos trigramas
+    # Comparacion de dos trigramas basada en el producto interno y la norma (distancia coseno)
     def compare(self, other):
         return self.iproduct(other)/(self.norm()*other.norm())
         
     """ Funciones Auxiliares """
     
+    # Genera el conjunto de ngramas de la palabra limpia de acentos y normalizada (sin repeticiones de ngramas) 
+    # El conjunto es representado como un conjunto de valores enteros (indices) de los ngramas
+    # El indice de un ngrama es calculado como la suma de los valores de los simbolos que 
+    # lo componen multiplicados por la base (cantidad de simbolos) elevado a la posicion del simbolo en el ngrama
     def ngrams(self, as_value=True):
 
         n = self.n
@@ -68,21 +74,13 @@ class Ngram:
 
         return ngrams
     
-    
-    # Obtiene una lista ordernada y sin duplicados de los trigramas 
-    def _getlist(self, other):
-
-        keyset = set(self.dvalues.keys())
-        keyset.update(other.dvalues.keys())
-        keylist = list(keyset)
-        keylist.sort()
-        return keylist
- 
-
+    # Genera el vector de valores flotantes de la secuencia de valores de ngramas
+    # cada valor del vector es la representacion de un bloque de n bits del vector binario 
+    # reconstruido a partir de el conjunto de valores de valores de ngramas (ngrams_values)
     def as_vector(self, n):
         # obtener la longitud de la secuencia
         sequence_length = self.symbol_count ** self.n
-        sparse_sequence = self.ngrams
+        sparse_sequence = self.ngrams_values
 
         vector_length = 1 + (sequence_length // n)
         # imprimir la longitud del vector
